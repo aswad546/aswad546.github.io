8093537f31db2b50749755caa26dd22d2045801b
Aswad Tariq <86660510+aswad546@users.noreply.github.com>
2024-06-04 21:05:26 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -6,9 +6,10 @@ function clickHandler(e) {
     let attr3 = e.isTrusted;
     let attr4 = e.target;
     let attr5 = e.currentTarget;
+    let oldValue = inputElement.value;
     let inputElement = document.querySelector('input');
     if (attr5 == 62) {return;}
-    else {inputElement.value = inputElement.value + attr1 + attr2 + attr3 + attr4 + attr5;}
+    else {inputElement.value = oldValue + attr1 + attr2 + attr3 + attr4 + attr5;}
 }
 
 document.addEventListener('mousemove', moveHandler);
@@ -22,6 +23,7 @@ function moveHandler(e) {
     let attr6 = e.type;
     let attr7 = e.movementX;
     let attr8 = e.movementY;
+    let oldValue = inputElement.value;
     let inputElement = document.querySelector('input');
-    inputElement.value = inputElement.value + attr1 + attr2 + attr3 + attr4 + attr5 + attr6 + attr7 + attr8;
+    inputElement.value = oldValue + attr1 + attr2 + attr3 + attr4 + attr5 + attr6 + attr7 + attr8;
 }
