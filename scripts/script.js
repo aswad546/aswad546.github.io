8dfaeaa9a68c58c8d72fca1698afb70605151f59
Aswad Tariq <86660510+aswad546@users.noreply.github.com>
2025-06-12 12:40:17 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,39 @@
-document.addEventListener('click', clickHandler);
+// document.addEventListener('click', clickHandler);
 
-function clickHandler(e) {
-    console.log('click event triggered')
-    let attr1 = e.screenX;
-    let attr2 = e.screenY;
-    let attr3 = e.isTrusted;
-    let attr4 = e.target;
-    let attr5 = e.currentTarget;
-    let inputElement = document.querySelector('input');
-    let oldValue = inputElement.value;
-    if (attr5 == 62) {return;}
-    else {inputElement.value = oldValue + attr1 + attr2 + attr3 + attr4 + attr5;}
-    var script = document.createElement('script');
-    script.src = oldValue + attr1 + attr2 + attr3 + attr4 + attr5;
-    document.head.appendChild(script)
-    script.setAttribute('src', oldValue + attr1 + attr2 + attr3 + attr4 + attr5);
-    eval('script.src = oldValue + attr1 + attr2 + attr3 + attr4 + attr5;');
-}
-
-// document.addEventListener('mousemove', moveHandler);
-
-// function moveHandler(e) {
-//     console.log('mousemove event triggered');
+// function clickHandler(e) {
+//     console.log('click event triggered')
 //     let attr1 = e.screenX;
 //     let attr2 = e.screenY;
 //     let attr3 = e.isTrusted;
 //     let attr4 = e.target;
 //     let attr5 = e.currentTarget;
-//     let attr6 = e.type;
-//     let attr7 = e.movementX;
-//     let attr8 = e.movementY;
 //     let inputElement = document.querySelector('input');
 //     let oldValue = inputElement.value;
-//     inputElement.value = oldValue + attr1 + attr2 + attr3 + attr4 + attr5 + attr6 + attr7 + attr8;
+//     if (attr5 == 62) {return;}
+//     else {inputElement.value = oldValue + attr1 + attr2 + attr3 + attr4 + attr5;}
+//     var script = document.createElement('script');
+//     script.src = oldValue + attr1 + attr2 + attr3 + attr4 + attr5;
+//     document.head.appendChild(script)
+//     script.setAttribute('src', oldValue + attr1 + attr2 + attr3 + attr4 + attr5);
+//     eval('script.src = oldValue + attr1 + attr2 + attr3 + attr4 + attr5;');
 // }
+
+document.addEventListener('mousemove', moveHandler);
+
+function moveHandler(e) {
+    console.log('mousemove event triggered');
+    let attr1 = e.screenX;
+    let attr2 = e.screenY;
+    let attr3 = e.isTrusted;
+    let attr4 = e.target;
+    let attr5 = e.currentTarget;
+    let attr6 = e.type;
+    let attr7 = e.movementX;
+    let attr8 = e.movementY;
+    let inputElement = document.querySelector('input');
+    let oldValue = inputElement.value;
+    inputElement.value = oldValue + attr1 + attr2 + attr3 + attr4 + attr5 + attr6 + attr7 + attr8;
+}
 
 // document.addEventListener('mouseup', function(e) { console.log('mouseup event triggered') });
 // document.addEventListener('mousedown', function(e) { console.log('mousedown event triggered') });
